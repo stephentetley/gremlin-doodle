fd38d0c08b52b9ba2cecdded45cea1734a36ca71
stephentetley <stephen.tetley@gmail.com>
2019-09-08 20:31:47 +0100

    Adding file missed from previous commit.

---
+++ src/Test03.fsx
@@ -0,0 +1,33 @@
+﻿
+
+// Start gremlin server with
+// > .\gremlin-server.bat
+
+open System.Collections.Generic
+
+#I @"C:\Users\stephen\.nuget\packages\gremlin.net\3.4.3\lib\netstandard2.0"
+#r @"Gremlin.Net.dll"
+open Gremlin.Net.Driver
+open Gremlin.Net.Driver.Remote
+open Gremlin.Net.Process.Traversal
+
+
+let loadData () : unit = 
+    let remoteConnection = new DriverRemoteConnection(new GremlinClient(new GremlinServer("localhost", 8182)))
+    let g = AnonymousTraversalSource.Traversal().WithRemote(remoteConnection)
+    let a = g.AddV("node").Property("id", "A").Next()
+    let b = g.AddV("node").Property("id", "B").Next()
+    g.V(a).AddE("hasParent").To(b).Iterate() |> ignore
+    remoteConnection.Dispose() |> ignore
+
+
+
+let demo01 ()  = 
+    let remoteConnection = new DriverRemoteConnection(new GremlinClient(new GremlinServer("localhost", 8182)))
+    let g = AnonymousTraversalSource.Traversal().WithRemote(remoteConnection)
+    let a = g.V().Has("node", "id", "A").Next()
+    remoteConnection.Dispose() |> ignore
+    a
+
+
+
